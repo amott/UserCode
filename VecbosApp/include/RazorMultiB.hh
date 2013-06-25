0a3e82970bc1ee2680d865584b5ae4a0dafc4314
woodson <>
2013-06-25 15:21:51 +0000

    forgot RazorMultiB.hh

--- VecbosApp/include/RazorMultiB.hh
+++ VecbosApp/include/RazorMultiB.hh
@@ -20,15 +20,24 @@ using namespace std;
 
 class RazorMultiB : public Vecbos{
 public:
-
   RazorMultiB(TTree *tree=0); /// Class Constructor
   RazorMultiB(TTree *tree=0, string jsonFile=string("none"), bool goodRunLS=false, bool isData=false); /// Class Constructor
+  RazorMultiB(TTree *tree=0, string jsonFile=string("none"), bool goodRunLS=false, bool isData=false, string smsName=string("none")); /// Class Constructor
   virtual ~RazorMultiB();     /// Class Destructor
   void Loop(string outFileName, int start, int stop);
   void SetConditions(TTree* treeCond);
   void SetWeight(double);
   double _weight;
   vector<Jet> FastJetAlgorithmForceTwo(vector<TLorentzVector> InputCollection, double Rparam=0.5, double thePtMin=1.);
+  double shat3D(TLorentzVector TOT, TVector3 P);
+  TVector3 Boost_type1(TLorentzVector H1, TLorentzVector H2);
+  TVector3 BetaTR(TLorentzVector TOT, TVector3 MET);
+  double shatR(TLorentzVector TOT, TVector3 MET);
+  vector<float> ParseEvent();
+  bool goodJetID(int i);
+  bool FailFilters();
+  bool isFlagged();
+  void InitEventFlag(char *s_Event);
 
 private:
   bool _isSMS;
@@ -36,6 +45,8 @@ private:
   bool _goodRunLS;
   TTree* _treeCond;
   struct JetConfig;
+  struct EventIndex;
+  map<EventIndex, int> EventCounts;
   JetConfig *theJetConfig;
   
 };
